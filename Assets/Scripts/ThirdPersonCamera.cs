ced05c2bb898f06d8078d675f57b24654800d7f3
kwmoore81 <kwmoore81@gmail.com>
2016-11-30 17:43:59 -0800

    First Push

---
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour {
+
+    public GameObject target;
+
+    void LateUpdate()
+    {
+        transform.LookAt(target.transform);
+    }
+}
