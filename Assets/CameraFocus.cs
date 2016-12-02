bac166bf3536dba6b5e443aba83d94af3c1b7fc6
kwmoore81 <kwmoore81@gmail.com>
2016-12-01 17:40:00 -0800

    Added Networking and Player Character

---
+++ Assets/CameraFocus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFocus : MonoBehaviour {
+
+    Camera mainCamera;
+    ThirdPersonCamera thirdPersonCamera;
+    public GameObject self;
+
+    // Use this for initialization
+    void Start () {
+        
+        thirdPersonCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<ThirdPersonCamera>();
+        thirdPersonCamera.target = gameObject;
+        mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        thirdPersonCamera.offset = gameObject.transform.position - mainCamera.transform.position;
+        
+    }
+	
+	
+}
--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -4,9 +4,28 @@ using System.Collections;
 public class ThirdPersonCamera : MonoBehaviour {
 
     public GameObject target;
+   // GameObject cameraFocus;
+
+    public float damping = 1;
+    public Vector3 offset;
+    
+    void Start()
+    {
+      
+        //offset = target.transform.position - transform.position;
+       
+    }
 
     void LateUpdate()
     {
+
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = target.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+
+        Quaternion rotation = Quaternion.Euler(0, angle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+
         transform.LookAt(target.transform);
     }
 }
